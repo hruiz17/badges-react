ca9ed141857813227eae3d78cea1c4a0862768b5
humruizr <humruizr@uis.edu.co>
2020-12-28 17:11:30 -0500

    update

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -24,7 +24,7 @@ class Badges extends React.Component {
         firstName: "Major",
         lastName: "Rodriguez",
         email: "Ilene66@hotmail.com",
-        jobTitle: "Human Research Architect",
+        jobTitle: "Arquitecto",
         twitter: "MajorRodriguez61545",
         avatarUrl:
           "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon",
@@ -35,7 +35,7 @@ class Badges extends React.Component {
         lastName: "Torphy",
         email: "Ron61@hotmail.com",
         jobTitle: "National Markets Officer",
-        twitter: "DaphneyTorphy96105",
+        twitter: "Daphney96105",
         avatarUrl:
           "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon",
       },
